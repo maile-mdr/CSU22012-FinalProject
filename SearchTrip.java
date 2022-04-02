217b367ba474cc48ac5bb4c16041506a1c85a5b5
Maile Monteiro da Rocha <82171829+maile-mdr@users.noreply.github.com>
2022-04-02 16:15:14 +0100

    Finished Part 3

--- SearchTrip.java
+++ SearchTrip.java
@@ -7,6 +7,12 @@ import java.util.*;
 
 public class SearchTrip {
 	
+	private static ArrayList<String> fullLine = new ArrayList<String>();
+	private static ArrayList<String> times = new ArrayList<String>();
+	
+	private static ArrayList<String> matchTimes = new ArrayList<String>();
+	private static ArrayList<String> matchLines = new ArrayList<String>();
+	
 	// Constants - used for error-checking
 	private static final int HRS_LOWER = 0;
 	private static final int HRS_UPPER = 23;
@@ -14,8 +20,6 @@ public class SearchTrip {
 	private static final int MINS_UPPER = 59;
 	private static final int SECS_LOWER = 0;
 	private static final int SECS_UPPER = 59;
-	
-	private static int stopNum = 0;
 
 	/**
     *
@@ -26,31 +30,29 @@ public class SearchTrip {
 		
 		String[] separateVal = input.split(":");
 		
-		int userHrs;
-		int userMins;
-		int userSecs;
+		int userHrs = 0;
+		int userMins = 0;
+		int userSecs = 0;
 		
 		// Source - https://www.freecodecamp.org/news/java-string-to-int-how-to-convert-a-string-to-an-integer/
 		// Checking for Input Validity: are the inputs integers?
 		try {
-			userHrs = Integer.parseInt(separateVal[0]);
-			userMins = Integer.parseInt(separateVal[1]);
-			userSecs = Integer.parseInt(separateVal[2]);
+				userHrs = Integer.parseInt(separateVal[0]);
+				userMins = Integer.parseInt(separateVal[1]);
+				userSecs = Integer.parseInt(separateVal[2]);
 		}
 		catch (NumberFormatException ex) {
-			System.out.println("Please enter a valid time format, with colons and no spaces (e.g. 11:47:58");
 			return false;
 		}
 		
 		// Checking for Input Validity: are the integers within bounds?
-		if ((userHrs >= HRS_LOWER && userHrs <= HRS_UPPER) && 
-			(userMins >= MINS_LOWER && userMins <= MINS_UPPER) &&
-			(userSecs >= SECS_LOWER && userSecs <= SECS_UPPER)) {
+		if (userHrs <= HRS_UPPER && 
+			userMins <= MINS_UPPER &&
+			userSecs <= SECS_UPPER) {
 			
 			return true;
 		}
 		else {
-			System.out.println("Your inputs are out of bounds! Please enter a time within 00:00:00 and 23:59:59");
 			return false;
 		}
 		
@@ -63,62 +65,72 @@ public class SearchTrip {
     * @param input: the time to be checked, as a string
     * @return ArrayList<String>: an ArrayList of Strings containing the lines which have valid times.
     */
-	public static ArrayList<String> cleanTimes(File filename) throws IOException {
+	public static void cleanTimes() throws IOException {
 		
-		BufferedReader buffRead = new BufferedReader(new FileReader(filename));
-		
-		String stopLine = "";
-		
-		ArrayList<String> cleanTimes = new ArrayList<String>();
-		
-		while ((stopLine = buffRead.readLine()) != null) {
+		try {
 			
-			String[] line = stopLine.split(",");
-			buffRead.close();
+			// turning file into array list
+			BufferedReader buffRead = new BufferedReader(new FileReader("stop_times.txt"));
 			
-			String stopTime = line[1];
-			String leaveTime = line[2];
-			
-			// if either of the stopTime or leaveTime is invalid, the whole line is deemed invalid
-			if (checkTime(stopTime) && checkTime(leaveTime)) {
-				cleanTimes.add(Arrays.toString(line));
+			String line;
+			line = buffRead.readLine();
+			while ((line = buffRead.readLine()) != null) {
+				String[] splitStop = line.split(",");
+				
+				String stopTime = splitStop[1];
+				stopTime = stopTime.replaceAll(" ", "");
+				
+				String leaveTime = splitStop[2];
+				leaveTime = leaveTime.replaceAll(" ", "");
+				
+				if (checkTime(stopTime) && checkTime(leaveTime)) {
+					times.add(stopTime);
+					fullLine.add(line);
+				}
 			}
+			
+			buffRead.close();
 		}
-		
-		buffRead.close();
-		
-		return cleanTimes;
+		catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
 		
 	}
 
 	// Find all stops matching a given arrival time
-	public static ArrayList<String> matchTimes(String input, ArrayList<String> cleanTimes) {
+	public static void matchTimes(String input) {
 		
-		ArrayList<String> matchTimes = new ArrayList<String>();
-		
-		return matchTimes;
+		for (int i = 0; i < times.size(); i++) {
+			if (input.equals(times.get(i))) {
+				matchTimes.add(times.get(i));
+				matchLines.add(fullLine.get(i));
+			}
+			
+		}
 	}
 	
 	// Sort the stopID in ascending order
-	public static void sortStops(ArrayList<String> matchTimes) {
+	public static void sortStops() {
 		
-		// gonna use bubble sort....
-		
+		Collections.sort(matchLines);
 	}
 	
 	// Mainline
 	public static void main(String[] args) throws IOException {
 		
-		File inputFile = new File("stop_times.txt");
-		ArrayList<String> cleanTimes = cleanTimes(inputFile);
-		
 		// Handle the user input
-		System.out.print("Please enter a valid time (e.g. 21:48:29): ");
-		String userInput = "";
+		String userInput = "15:00:00";
+
+		cleanTimes();
+		matchTimes(userInput);
+		sortStops();
 		
-		sortStops(matchTimes(userInput, cleanTimes));
+		System.out.println(matchTimes.size());
 		
 		// Output the sorted, matching times.
+		for (int i = 0; i < matchTimes.size(); i++) {
+			System.out.println(matchLines.get(i));
+		}
 		
 	}
 
